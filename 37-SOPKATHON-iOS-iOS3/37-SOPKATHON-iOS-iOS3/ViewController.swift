4044708815fbd23f6180d8b77c6c548205827df8
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-21 16:11:06 +0900

    init: #1 프로젝트 세팅

---
+++ 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  37-SOPKATHON-iOS-iOS3
+//
+//  Created by 이나연 on 11/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
