45c4459be75d15674e000ae19a6853c3d5a597d8
sum130 <sm3904@naver.com>
2025-11-23 05:08:52 +0900

    [feat]이전다음버튼 컴포넌트 글받기 뷰

--- 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/PagingButton.swift
+++ 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/PagingButton.swift
@@ -13,9 +13,14 @@ final class PagingButton: UIButton {
     
     enum ButtonStyle {
         case enabledBack
-        case enabledNext
-        case disabledBack
         case disabledNext
+        case clickedComplete
+        case complete
+        
+        
+        case receivedBack
+        
+        case receiveNext
     }
     
     init(style: ButtonStyle) {
@@ -28,37 +33,47 @@ final class PagingButton: UIButton {
     }
     
     private func setupStyle(style: ButtonStyle) {
-        
         self.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
         self.layer.cornerRadius = 10
         self.clipsToBounds = true
         
         switch style {
         case .enabledBack:
-            self.backgroundColor = .grey400.withAlphaComponent(0.2)
-            self.backgroundColor?.withAlphaComponent(0.2)
-            self.setTitleColor(.grey300, for: .normal)
-            self.setTitle("이전", for: .normal)
-            self.layer.borderColor = UIColor.grey400.cgColor
-            self.layer.borderWidth = 1
-        case .enabledNext:
-            self.backgroundColor = .primary
-            self.setTitleColor(.mainBlack, for: .normal)
-            self.setTitle("완료", for: .normal)
-        case .disabledBack:
-            self.backgroundColor = .grey400.withAlphaComponent(0.2)
-            self.setTitleColor(.grey400, for: .normal)
+            self.backgroundColor = .white
+            self.setTitleColor(.grey400, for: .normal)//하나는 클릭될 때 하이라이트로 설정해서.
             self.setTitle("이전", for: .normal)
             self.layer.borderColor = UIColor.grey400.cgColor
             self.layer.borderWidth = 1
         case .disabledNext:
-            self.backgroundColor = .grey300.withAlphaComponent(0.2)
+            self.backgroundColor = .grey300
             self.setTitleColor(.grey400, for: .normal)
+            self.setTitle("다음", for: .normal)
+            self.layer.borderColor = UIColor.grey400.cgColor
+            self.layer.borderWidth = 1
+            
+        case .complete:
+            self.backgroundColor = .primary
+            self.setTitleColor(.mainBlack, for: .normal)
+            self.setTitle("완료", for: .normal)
+        case .receivedBack:
+            self.backgroundColor = .grey800
+            self.setTitleColor(.grey400, for: .normal)
+            self.setTitle("이전", for: .normal)
+            self.layer.borderColor = UIColor.grey400.cgColor
+            self.layer.borderWidth = 1
+        case .receiveNext:
+            self.backgroundColor = .primary
+            self.setTitleColor(.mainBlack, for: .normal)
+            self.setTitle("들어볼게요", for: .normal)
+            
+        case .clickedComplete:
+            self.backgroundColor = .subColor2
+            self.setTitleColor(.mainBlack, for: .highlighted)
             self.setTitle("완료", for: .normal)
         }
     }
     
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 160, height: 48)
+        return CGSize(width: UIView.noIntrinsicMetric, height: 48)
     }
 }
--- 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/receiveArticleView.swift
+++ 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/receiveArticleView.swift
@@ -10,134 +10,107 @@ import UIKit
 import SnapKit
 import Then
 
-class receiveArticleView: BaseView {
+class ReceiveArticleView: BaseView {
     
     // MARK: - UI
     private let announceLabel = UILabel()
     private let elfImageView = UIImageView()
-    private let mearyImageView = UIImageView()
-    private let backButton = UIButton(type: .system)
-    private let nextButton = UIButton(type: .system)
-    private let textBubbleView = UIImageView()//말풍선
+    private let echoImageView = UIImageView()
+    private let textBubbleView = UIImageView()
+    private let buttonStackView = UIStackView()
     
-    // MARK: - Init
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setStyle()
-        setUI()
-        setLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+    private let backButton = PagingButton(style: .receivedBack)
+    private let nextButton = PagingButton(style: .receiveNext)
     
     // MARK: - SetUI
     override func setUI() {
         
         addSubviews(
             announceLabel,
-            mearyImageView,
+            echoImageView,
             elfImageView,
             textBubbleView,
-            backButton,
-            nextButton,
+            buttonStackView
         )
+        
+        buttonStackView.addArrangedSubviews(backButton, nextButton)
     }
     
     // MARK: - SetStyle
     override func setStyle() {
+        backgroundColor = .mainBlack
+        
         elfImageView.do {
-            $0.image = UIImage(named: "elf")
+            $0.image = UIImage(named: "receive_img_elfi")
             $0.contentMode = .scaleAspectFit
         }
         
-        mearyImageView.do {
-            $0.image = UIImage(named: "meary")
+        echoImageView.do {
+            $0.image = UIImage(named: "echo")
             $0.contentMode = .scaleAspectFit
         }
-        announceLabel.do {
-            $0.text = "ㅇㅇ님이 작성한 글 잘 받았습니다.\n다른 사람의 메아리도 들어볼래요?"
-            $0.textAlignment = .center
-            $0.numberOfLines = 0
+        let fullText = "이나님 이 작성한 글 잘 받았습니다.\n다른 사람의 메아리도 들어볼래요?"
+        let attributedString = NSMutableAttributedString(string: fullText)
+        attributedString.addAttribute(
+            .foregroundColor,
+            value: UIColor.white,
+            range: NSRange(fullText.startIndex..<fullText.endIndex, in: fullText))
+        
+        if let range = fullText.range(of: "이나님") {
+            let nsRange = NSRange(range, in: fullText)
+            attributedString.addAttribute(
+                .foregroundColor,
+                value: UIColor.primary,
+                range: nsRange)
+            attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 20, weight: .bold), range: nsRange)
         }
+        announceLabel.attributedText = attributedString
+        announceLabel.textAlignment = .center
+        announceLabel.numberOfLines = 0
         textBubbleView.do {
-            $0.image = UIImage(named: "textBubble")
+            $0.image = UIImage(named: "bubble")
             $0.contentMode = .scaleAspectFit
         }
         
-        backButton.do {
-            $0.setTitle("이전", for: .normal)
-            $0.setTitleColor(.black, for: .normal)
-            $0.layer.cornerRadius = 10
-            $0.backgroundColor = .white
-            $0.clipsToBounds = true
+        buttonStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 10
+            $0.distribution = .fillEqually
         }
-
-        nextButton.do {
-            $0.setTitle("다음", for: .normal)
-            $0.setTitleColor(.white, for: .normal)
-            $0.backgroundColor = .darkGray
-            $0.layer.cornerRadius = 10
-            $0.clipsToBounds = true
-        }
-
         
-
     }
     
     // MARK: - Layout
     override func setLayout() {
-        
         announceLabel.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
-            $0.top.equalToSuperview().offset(115)
+            $0.top.equalToSuperview().offset(156)
         }
-
-        elfImageView.snp.makeConstraints {
-            $0.centerY.equalTo(textBubbleView.snp.centerY)
-            $0.leading.equalTo(textBubbleView.snp.trailing).offset(-10)
-        }
-
-        
-        mearyImageView.snp.makeConstraints {
-            $0.top.equalTo(announceLabel.snp.bottom).offset(35)
+        echoImageView.snp.makeConstraints {
+            $0.top.equalTo(announceLabel.snp.bottom).offset(23)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(240)
-            $0.height.equalTo(235)
-
+            $0.width.equalTo(236)
+            $0.height.equalTo(240)
+            
         }
         
         elfImageView.snp.makeConstraints {
-            $0.centerY.equalTo(textBubbleView.snp.centerY)
-            $0.leading.equalTo(textBubbleView.snp.trailing).offset(-10)
+            $0.top.equalTo(echoImageView.snp.bottom).offset(31)
+            $0.trailing.equalToSuperview().offset(-44)
         }
         
         textBubbleView.snp.makeConstraints {
-            $0.top.equalTo(mearyImageView.snp.bottom).offset(60)
-            $0.centerX.equalToSuperview()
+            $0.top.equalTo(echoImageView.snp.bottom).offset(53)
+            $0.leading.equalToSuperview().offset(54)
         }
         
-        backButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(24)
-            $0.bottom.equalToSuperview().offset(-40)
-            $0.width.equalTo(80)
-            $0.height.equalTo(40)
+        buttonStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(22)
+            $0.bottom.equalToSuperview().inset(40)
         }
-
-        nextButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().offset(-24)
-            $0.bottom.equalToSuperview().offset(-40)
-            $0.width.equalTo(80)
-            $0.height.equalTo(40)
-        }
-
-
     }
 }
 
 #Preview {
-    receiveArticleView()
+    ReceiveArticleView()
 }
