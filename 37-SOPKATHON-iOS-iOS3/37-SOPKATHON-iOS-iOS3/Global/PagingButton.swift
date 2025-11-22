165a32a730518a4790e507e7d6abbab41b15b9fb
sum130 <sm3904@naver.com>
2025-11-23 03:31:47 +0900

    [fix]페이징버튼 수정

--- 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/PagingButton.swift
+++ 37-SOPKATHON-iOS-iOS3/37-SOPKATHON-iOS-iOS3/Global/PagingButton.swift
@@ -9,28 +9,56 @@ import UIKit
 import SnapKit
 import Then
 
-
 final class PagingButton: UIButton {
     
-    init(title: String, titleColor: UIColor, backgroundColor: UIColor) {
+    enum ButtonStyle {
+        case enabledBack
+        case enabledNext
+        case disabledBack
+        case disabledNext
+    }
+    
+    init(style: ButtonStyle) {
         super.init(frame: .zero)
-        setupStyle(title: title, titleColor: titleColor, backgroundColor: backgroundColor)
+        setupStyle(style: style)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupStyle(title: String, titleColor: UIColor, backgroundColor: UIColor) {
-        self.setTitle(title, for: .normal)
-        self.setTitleColor(titleColor, for: .normal)
-        self.backgroundColor = backgroundColor
+    private func setupStyle(style: ButtonStyle) {
+        
+        self.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
         self.layer.cornerRadius = 10
         self.clipsToBounds = true
-        self.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        
+        switch style {
+        case .enabledBack:
+            self.backgroundColor = .grey400.withAlphaComponent(0.2)
+            self.backgroundColor?.withAlphaComponent(0.2)
+            self.setTitleColor(.grey300, for: .normal)
+            self.setTitle("이전", for: .normal)
+            self.layer.borderColor = UIColor.grey400.cgColor
+            self.layer.borderWidth = 1
+        case .enabledNext:
+            self.backgroundColor = .primary
+            self.setTitleColor(.mainBlack, for: .normal)
+            self.setTitle("완료", for: .normal)
+        case .disabledBack:
+            self.backgroundColor = .grey400.withAlphaComponent(0.2)
+            self.setTitleColor(.grey400, for: .normal)
+            self.setTitle("이전", for: .normal)
+            self.layer.borderColor = UIColor.grey400.cgColor
+            self.layer.borderWidth = 1
+        case .disabledNext:
+            self.backgroundColor = .grey300.withAlphaComponent(0.2)
+            self.setTitleColor(.grey400, for: .normal)
+            self.setTitle("완료", for: .normal)
+        }
     }
     
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 173, height: 48)
+        return CGSize(width: 160, height: 48)
     }
 }
